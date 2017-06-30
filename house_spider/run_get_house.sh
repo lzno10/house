0063f3684cffe314b31c09e5d435c4898120ee83
liuzhuo <liuzhuo@360.cn>
2017-06-30 19:14:55 +0800

    add comments

--- house_spider/run_get_house.sh
+++ house_spider/run_get_house.sh
@@ -5,6 +5,11 @@ CUR_DIR=`dirname "${CUR_DIR}"`
 cd ${CUR_DIR}
 export PATH=${HOME}/bin:/usr/local/bin:/home/s/bin:${CUR_DIR}/env/bin:${PATH}
 
+echo `date`" begin get data"
 python get_lianjia.py
+echo `date`"finish get data"
+
 echo "cp data/house.data /usr/share/nginx/html/house_stat/"
-cp data/house.data /usr/share/nginx/html/house_stat/
+\cp data/house.data /usr/share/nginx/html/house_stat/
+\cp data/house.data data/house.data.`date +%Y%m%d`
+echo `date`" finish\n\n"
