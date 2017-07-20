7af72e8e0268f4113940816a21fc8e9a19ce5174
liuzhuo <liuzhuo@360.cn>
2017-07-20 22:31:12 +0800

    m sync.sh

--- sync.sh
+++ sync.sh
@@ -5,5 +5,13 @@ CUR_DIR=`dirname "${CUR_DIR}"`
 cd ${CUR_DIR}
 export PATH=${HOME}/bin:/usr/local/bin:/home/s/bin:${CUR_DIR}/env/bin:${PATH}
 
+git ci -am "$1"
+git push -u origin master
 rm -rf /usr/share/nginx/html/house_stat/*
 \cp -r ./house_stat/* /usr/share/nginx/html/house_stat/
+echo -e "\n-----rsync-----"
+rsync -rvt house_stat/ /usr/share/nginx/html/house_stat/
+
+echo -e "\n-----check delete-----"
+rsync -rvtn --delete house_stat/ /usr/share/nginx/html/house_stat/
+echo "rsync -rvt --delete house_stat/ /usr/share/nginx/html/house_stat/"
