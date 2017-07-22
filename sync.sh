55a3a02bfe48d75411f24000e099536ba4906727
liuzhuo <liuzhuo@360.cn>
2017-07-22 17:07:27 +0800

    modify sync.sh

--- sync.sh
+++ sync.sh
@@ -5,23 +5,11 @@ CUR_DIR=`dirname "${CUR_DIR}"`
 cd ${CUR_DIR}
 export PATH=${HOME}/bin:/usr/local/bin:/home/s/bin:${CUR_DIR}/env/bin:${PATH}
 
-#git ci -am "$1"
-#git push -u origin master
-#rm -rf /usr/share/nginx/html/house_stat/*
-#\cp -r ./house_stat/* /usr/share/nginx/html/house_stat/
-#echo -e "\n-----rsync-----"
-#rsync -rvt house_stat/ /usr/share/nginx/html/house_stat/
-
-#echo -e "\n-----check delete-----"
-#rsync -rvtn --delete house_stat/ /usr/share/nginx/html/house_stat/
-#echo "rsync -rvt --delete house_stat/ /usr/share/nginx/html/house_stat/"
-
-
 program="house_stat"
 exclude_list=".exclude.list"
 src_dir="/home/liuzhuo/project/house"
-dest_user="liuzhuo"
-dest_host="localhost"
+dest_user=""
+dest_host=""
 dest_dir="/usr/share/nginx/html/"
 
 update_git() {
@@ -34,9 +22,14 @@ if [ "${1}"x != ""x ];then
     echo "git push -u origin master"
     git push -u origin master
 fi
+
+if [ "${dest_host}"x != ""x ];then
+    dest_dir=${dest_user}@${dest_host}:${dest_dir}
+fi
 echo -e "\n-----run rsync-----"
-echo "rsync -vtr --exclude-from=${exclude_list} ${src_dir}/${program} ${dest_user}@${dest_host}:${dest_dir}"
-rsync -vtr --exclude-from=${exclude_list} ${src_dir}/${program} ${dest_user}@${dest_host}:${dest_dir}
+echo "rsync -vtr --exclude-from=${exclude_list} ${src_dir}/${program} ${dest_dir}"
+rsync -vtr --exclude-from=${exclude_list} ${src_dir}/${program} ${dest_dir}
+
 echo -e "\n-----check delete-----"
-echo "rsync -vtrn --delete --exclude-from=${exclude_list} ${src_dir}/${program} ${dest_user}@${dest_host}:${dest_dir}"
-rsync -vtrn --delete --exclude-from=${exclude_list} ${src_dir}/${program} ${dest_user}@${dest_host}:${dest_dir}
+echo "rsync -vtrn --delete --exclude-from=${exclude_list} ${src_dir}/${program} ${dest_dir}"
+rsync -vtrn --delete --exclude-from=${exclude_list} ${src_dir}/${program} ${dest_dir}
