83d1203d7108243fb0cc1110a711049a9c1a9b98
liuzhuo <liuzhuo@360.cn>
2017-07-22 19:12:54 +0800

    modify cp dir

--- house_spider/run_get_house.sh
+++ house_spider/run_get_house.sh
@@ -9,7 +9,7 @@ echo `date`" begin get data"
 python get_lianjia.py
 echo `date`"finish get data"
 
-echo "cp data/house.data /usr/share/nginx/html/house_stat/"
-\cp data/house.data /usr/share/nginx/html/house_stat/
+echo "cp data/house.data /usr/share/nginx/html/house/"
+\cp data/house.data /usr/share/nginx/html/house/
 \cp data/house.data data/house.data.`date +%Y%m%d`
 echo -e `date`" finish\n\n"
